44b9dc246da7be1c35c8689f2767ee3765872290
falz13 <144127842+falz13@users.noreply.github.com>
2025-11-10 15:54:13 +0000

    update to include hashmap

--- client/java/com/jamsackman/researchtable/ResearchTableClient.java
+++ client/java/com/jamsackman/researchtable/ResearchTableClient.java
@@ -22,12 +22,7 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class ResearchTableClient implements ClientModInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger("ResearchTableClient");
